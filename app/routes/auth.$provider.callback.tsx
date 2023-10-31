5e9983727b14c7ec987f5bbe400dae30fcd82f9e
Devin <devin.rasmussen@icloud.com>
2023-10-31 13:57:24 -0600

    create user session if user is verified social login

--- app/routes/auth.$provider.callback.tsx
+++ app/routes/auth.$provider.callback.tsx
@@ -1,13 +1,24 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
+import { FacebookProfile, SocialsProvider } from "remix-auth-socials";
 
-import { authenticator } from "~/session.server";
+import { verifySocialLogin } from "~/models/user.server";
+import { authenticator, createUserSession } from "~/session.server";
 
-export const loader = ({ request, params }: LoaderFunctionArgs) => {
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   if (typeof params.provider !== "string") {
     throw new Error("Expected 'provider' to be a string");
   }
-  return authenticator.authenticate(params.provider, request, {
-    successRedirect: "/",
+  const profile = (await authenticator.authenticate(params.provider, request, {
     failureRedirect: "/login",
+  })) as FacebookProfile;
+  const user = await verifySocialLogin(SocialsProvider.FACEBOOK, profile.id);
+  if (!user) {
+    throw Error("User not found, despite being authenticated?");
+  }
+  return createUserSession({
+    redirectTo: "/",
+    remember: true,
+    request,
+    userId: user.id,
   });
 };

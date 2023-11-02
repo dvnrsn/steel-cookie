e512f2cf8c2f523202ab8cc41b41ceefeb02d273
Devin <devin.rasmussen@icloud.com>
2023-11-02 12:20:52 -0600

    Implement search

--- app/routes/songs._index.tsx
+++ app/routes/songs._index.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData, useSubmit } from "@remix-run/react";
+import { useState } from "react";
 
 import { getSongListItems } from "~/models/song.server";
 
@@ -14,35 +15,66 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export default function NotesPage() {
   const data = useLoaderData<typeof loader>();
+  const [search, setSearch] = useState("");
+  const submit = useSubmit();
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Song Link</th>
-          <th>Tutorial Link</th>
-          <th>Spotify Link</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.noteListItems.map((song) => (
-          <tr key={song.id}>
-            <td className="p-2 border-indigo-200 border-solid border-b-2">
-              {song.name}
-            </td>
-            <td className="p-2 border-indigo-200 border-solid border-b-2">
-              {song.songLink}
-            </td>
-            <td className="p-2 border-indigo-200 border-solid border-b-2">
-              {song.tutorialLink}
-            </td>
-            <td className="p-2 border-indigo-200 border-solid border-b-2">
-              {song.spotifyLink}
-            </td>
+    <>
+      <div className="flex">
+        <Form
+          onChange={(event) => {
+            submit(event.currentTarget);
+          }}
+        >
+          <input
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+            type="search"
+            className="p-2"
+            placeholder="Search"
+            name="search"
+          />
+        </Form>
+        <Link to="new" className="block p-2">
+          + New Song
+        </Link>
+      </div>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.noteListItems.map((song) => (
+            <tr key={song.id}>
+              <td className="p-2 border-black-200 border-solid border-b-2">
+                {song.title}
+              </td>
+              <td className="p-2 border-black-200 border-solid border-b-2">
+                <Link to={`${song.songLink}`}>song</Link>
+              </td>
+              <td className="p-2 border-black-200 border-solid border-b-2">
+                <Link to={`${song.tutorialLink}`}>tutorial</Link>
+              </td>
+              <td className="p-2 border-black-200 border-solid border-b-2">
+                <Link to={`${song.spotifyLink}`}>
+                  <img
+                    alt="spotify"
+                    src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-Logo.png"
+                    className="max-h-6"
+                  />
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }

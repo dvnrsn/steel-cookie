a2e3ca1e45e20ab6c926d8b498f6e160bf41536e
Devin <devin.rasmussen@icloud.com>
2023-11-04 16:00:07 -0600

    load app immediately to /songs for now

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+
+export const loader = () => {
+  return redirect("/songs");
+};
 
 export default function Index() {
   const user = useOptionalUser();

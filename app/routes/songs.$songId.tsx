b55f2b975ad74114143d23739697da20b51fcd2f
Devin <devin.rasmussen@icloud.com>
2023-11-07 17:31:39 -0700

    Style and center view song page

--- app/routes/songs.$songId.tsx
+++ app/routes/songs.$songId.tsx
@@ -38,106 +38,118 @@ export default function SongDetailsPage() {
   const user = useOptionalUser();
 
   return (
-    <div className="relative w-max">
-      <Link to=".." className="block p-2 md:absolute md:-translate-x-14">
-        <BsArrowLeft size={24} />
-      </Link>
-      {user?.isAdmin ? (
-        <Link
-          to="edit"
-          className="block right-0 p-2 md:absolute md:translate-x-14"
-        >
-          <BsPencil size={24} />
-        </Link>
-      ) : null}
-      <h3 className="text-2xl font-bold">{song.title}</h3>
-      <div>
-        <h1>
-          {song.title} by {song.artist}
-        </h1>
-        {song.songLink ? (
-          <a
-            href={song.songLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block"
+    <div className="w-full flex justify-center mt-6">
+      <div className="relative w-max flex-col justify-center">
+        <div className="flex justify-between">
+          <Link
+            to=".."
+            className="block p-2 md:absolute md:-translate-x-14 hover:bg-slate-200 rounded-lg"
           >
-            Listen to the Song
-          </a>
-        ) : null}
-        {song.spotifyLink ? (
-          <a href={song.spotifyLink} target="_blank" rel="noopener noreferrer">
-            Listen on Spotify
-          </a>
-        ) : null}
+            <BsArrowLeft size={24} />
+          </Link>
+          {user?.isAdmin ? (
+            <Link
+              to="edit"
+              className="block right-0 p-2 md:absolute md:translate-x-14 hover:bg-slate-200 rounded-lg"
+            >
+              <BsPencil size={24} />
+            </Link>
+          ) : null}
+        </div>
+        <h3 className="text-2xl font-bold">{song.title}</h3>
+        <div>
+          <h1>
+            {song.title} by {song.artist}
+          </h1>
+          {song.songLink ? (
+            <a
+              href={song.songLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block"
+            >
+              Listen to the Song
+            </a>
+          ) : null}
+          {song.spotifyLink ? (
+            <a
+              href={song.spotifyLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Listen on Spotify
+            </a>
+          ) : null}
 
-        <h2 className="mt-4">Dance name: {song.danceName}</h2>
-        {song.danceInstructionsLink ? (
-          <a
-            href={song.danceInstructionsLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block"
-          >
-            Dance Instructions
-          </a>
-        ) : null}
-        <p>Choreographer: {song.danceChoreographer}</p>
+          <h2 className="mt-4">Dance name: {song.danceName}</h2>
+          {song.danceInstructionsLink ? (
+            <a
+              href={song.danceInstructionsLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block"
+            >
+              Dance Instructions
+            </a>
+          ) : null}
+          <p>Choreographer: {song.danceChoreographer}</p>
 
-        {song.stepSheetLink ? (
-          <a
-            href={song.stepSheetLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block"
-          >
-            Step Sheet
-          </a>
-        ) : null}
+          {song.stepSheetLink ? (
+            <a
+              href={song.stepSheetLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block"
+            >
+              Step Sheet
+            </a>
+          ) : null}
 
-        <p>Dance Counts: {song.danceCounts}</p>
-        <p>Wall Counts: {song.wallCounts}</p>
-        <p>Starting Weight Foot: {song.startingWeightFoot}</p>
+          <p>Dance Counts: {song.danceCounts}</p>
+          <p>Wall Counts: {song.wallCounts}</p>
+          <p>Starting Weight Foot: {song.startingWeightFoot}</p>
 
-        {user ? (
-          <>
-            {song?.createdAt ? (
-              <p className="mt-4">
-                Added By: {song.createdBy?.firstName || song.createdBy?.email}
-                {" - "}
-                {new Date(song.createdAt).toLocaleDateString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-              </p>
-            ) : null}
+          {user ? (
+            <>
+              {song?.createdAt ? (
+                <p className="mt-4">
+                  Added By: {song.createdBy?.firstName || song.createdBy?.email}
+                  {" - "}
+                  {new Date(song.createdAt).toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+              ) : null}
 
-            {song?.updatedAt ? (
-              <p>
-                Updated By: {song.updatedBy?.firstName || song.updatedBy?.email}
-                {" - "}
-                {new Date(song?.updatedAt).toLocaleDateString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-              </p>
-            ) : null}
-          </>
+              {song?.updatedAt ? (
+                <p>
+                  Updated By:{" "}
+                  {song.updatedBy?.firstName || song.updatedBy?.email}
+                  {" - "}
+                  {new Date(song?.updatedAt).toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+              ) : null}
+            </>
+          ) : null}
+        </div>
+
+        {user?.isAdmin ? (
+          <Form method="post">
+            <button
+              type="submit"
+              className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
+            >
+              Delete
+            </button>
+          </Form>
         ) : null}
       </div>
-
-      {user?.isAdmin ? (
-        <Form method="post">
-          <button
-            type="submit"
-            className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
-          >
-            Delete
-          </button>
-        </Form>
-      ) : null}
     </div>
   );
 }
